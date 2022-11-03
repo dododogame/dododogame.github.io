e5362567403040ad8abae242bf9d798eb1dc177c
UlyssesZh <UlyssesZh@users.noreply.github.com>
2022-11-03 05:48:15 +0000

    jekyll build from Action 369e3fba6dfff8012630711c7850d023c7fa1ab9

---
+++ dododo/js/Scene_Base.js
@@ -0,0 +1,62 @@
+function Scene_Base() {
+	this.initialize.apply(this, arguments);
+}
+
+Scene_Base.prototype = Object.create(Stage.prototype);
+Scene_Base.prototype.constructor = Scene_Base;
+
+Scene_Base.prototype.initialize = function () {
+	Stage.prototype.initialize.call(this);
+};
+
+Scene_Base.prototype.start = function () {
+	this._backgroundSprite = new Sprite(new Bitmap(Graphics.width, Graphics.height));
+	this._backgroundSprite.bitmap.fillAll(preferences.backgroundColor);
+	this.addChild(this._backgroundSprite);
+	this._shouldTakeScreenShot = false;
+	this._globalHotkeyListener = this._globalOnKeyDown.bind(this);
+	document.addEventListener('keydown', this._globalHotkeyListener);
+};
+
+Scene_Base.prototype.update = function() {
+	this.updateChildren();
+	if (this._shouldTakeScreenShot) {
+		Graphics.snapshotToClipboard();
+		this._shouldTakeScreenShot = false;
+	}
+	if (window.scene !== this) {
+		this.stop();
+		window.scene.start();
+	}
+};
+
+Scene_Base.prototype.stop = function () {
+	document.removeEventListener('keydown', this._globalHotkeyListener);
+};
+
+Scene_Base.prototype._globalOnKeyDown = function (event) {
+	if (!event.ctrlKey && !event.altKey) {
+		if (event.key === 'F9') {
+			this._shouldTakeScreenShot = true;
+		} else if (window.nw && event.key === 'F5') {
+			location.reload();
+		} else if (window.nw && event.key === 'F12') {
+			nw.Window.get().showDevTools();
+		}
+	}
+};
+
+Scene_Base.prototype.updateChildren = function () {
+	this.children.forEach(child => {
+		if (child.update) child.update();
+	});
+};
+
+Scene_Base.prototype._center = function (sprite, y) {
+	sprite.anchor.x = 0.5;
+	sprite.x = Graphics.width / 2;
+	sprite.y = y;
+};
+
+Scene_Base.prototype.onrender = function () {
+};
